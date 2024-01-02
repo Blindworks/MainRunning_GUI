e66b46f3b30a4b506aa9546aa11dd7dfb62e678f
Benedikt Lind <benedikt.lind@lhind.dlh.de>
2024-01-02 20:40:08 +0100

    Navbar added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {AngularSvgIconModule} from 'angular-svg-icon';
+import {NavbarComponent} from './layout/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
