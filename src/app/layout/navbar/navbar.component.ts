1bb33eddc53004d74a17bb10ba7988c5d6dde316
Benedikt Lind <benedikt.lind@lhind.dlh.de>
2024-01-04 12:17:22 +0100

    Run upload button added

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
     MatMenuModule,
+    MatButtonModule,
+    MatTooltipModule,
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
