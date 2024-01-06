a0f06b809f642417b8b46eb232caefceca5823c0
Benedikt Lind <benedikt.lind@lhind.dlh.de>
2024-01-06 10:42:33 +0100

    Sign up path changed

--- src/app/authentication/auth.routes.ts
+++ src/app/authentication/auth.routes.ts
@@ -1,8 +1,8 @@
 import {Routes} from '@angular/router';
 
 export const AuthRoutes: Routes = [
-  { path: 'signup', loadComponent: () => import('../authentication/sign-up/sign-up.component').then((x) => x.SignUpComponent)},
+  { path: 'sign-up', loadComponent: () => import('../authentication/sign-up/sign-up.component').then((x) => x.SignUpComponent)},
   //{ path: 'donuts/new', loadComponent: () => import('../admin/containers/donut-single/donut-single.component').then((x) => x.DonutSingleComponent), data: { isEdit: false} },
   //{ path: 'donuts/:id', loadComponent: () => import('../admin/containers/donut-single/donut-single.component').then((x) => x.DonutSingleComponent), data: { isEdit: true} },
-  { path: '', pathMatch: 'full', redirectTo: 'signup'}
+  { path: '', pathMatch: 'full', redirectTo: 'sign-up'}
 ]
