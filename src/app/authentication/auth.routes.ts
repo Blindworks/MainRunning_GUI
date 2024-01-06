97851f5403ee25f55d4901929096c98fdd71f415
Benedikt Lind <benedikt.lind@lhind.dlh.de>
2024-01-06 11:49:50 +0100

    Sign in component implemenetd

--- src/app/authentication/auth.routes.ts
+++ src/app/authentication/auth.routes.ts
@@ -2,6 +2,7 @@ import {Routes} from '@angular/router';
 
 export const AuthRoutes: Routes = [
   { path: 'sign-up', loadComponent: () => import('../authentication/sign-up/sign-up.component').then((x) => x.SignUpComponent)},
+  { path: 'sign-in', loadComponent: () => import('../authentication/sign-in/sign-in.component').then((x) => x.SignInComponent)},
   //{ path: 'donuts/new', loadComponent: () => import('../admin/containers/donut-single/donut-single.component').then((x) => x.DonutSingleComponent), data: { isEdit: false} },
   //{ path: 'donuts/:id', loadComponent: () => import('../admin/containers/donut-single/donut-single.component').then((x) => x.DonutSingleComponent), data: { isEdit: true} },
   { path: '', pathMatch: 'full', redirectTo: 'sign-up'}
