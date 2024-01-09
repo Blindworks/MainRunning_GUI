0babfab497235cd94bda18f7818f18bba460f7cb
Benedikt Lind <benedikt.lind@lhind.dlh.de>
2024-01-09 17:07:29 +0100

    Refresh JWT token implemented

--- src/app/helpers/http.interceptor.ts
+++ src/app/helpers/http.interceptor.ts
@@ -1,21 +1,71 @@
 import {
-  HTTP_INTERCEPTORS,
+  HTTP_INTERCEPTORS, HttpErrorResponse,
   HttpEvent,
   HttpHandler,
   HttpInterceptor,
   HttpRequest
 } from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import {catchError, Observable, switchMap, throwError} from 'rxjs';
+import {StorageService} from '../authentication/service/storage.service';
+import {AuthService} from '../authentication/service/auth.service';
+import {EventBusService} from '../authentication/service/event-bus.service';
+import {EventData} from '../authentication/service/event-data';
 
 @Injectable()
 export class HttpRequestInterceptor implements HttpInterceptor {
+  private isRefreshing = false;
+
+  constructor(
+    private storageService: StorageService,
+    private authService: AuthService,
+    private eventBusService: EventBusService
+  ) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     req = req.clone({
       withCredentials: true,
     });
 
-    return next.handle(req);
+    return next.handle(req).pipe(
+      catchError((error) => {
+        if (
+          error instanceof HttpErrorResponse &&
+          !req.url.includes('auth/signin') &&
+          error.status === 401
+        ) {
+          return this.handle401Error(req, next);
+        }
+
+        return throwError(() => error);
+      })
+    );
+  }
+  private handle401Error(request: HttpRequest<any>, next: HttpHandler) {
+    if (!this.isRefreshing) {
+      this.isRefreshing = true;
+
+      if (this.storageService.isLoggedIn()) {
+        return this.authService.refreshToken().pipe(
+          switchMap(() => {
+            this.isRefreshing = false;
+
+            return next.handle(request);
+          }),
+          catchError((error) => {
+            this.isRefreshing = false;
+
+            if (error.status == '403') {
+              this.eventBusService.emit(new EventData('logout', null));
+            }
+
+            return throwError(() => error);
+          })
+        );
+      }
+    }
+
+    return next.handle(request);
   }
 }
 
--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -6,6 +6,8 @@ import {RouterLink} from '@angular/router';
 import {StorageService} from '../../authentication/service/storage.service';
 import {NgIf} from '@angular/common';
 import {AuthService} from '../../authentication/service/auth.service';
+import {Subscription} from 'rxjs';
+import {EventBusService} from '../../authentication/service/event-bus.service';
 
 @Component({
   selector: 'app-navbar',
@@ -23,13 +25,19 @@ import {AuthService} from '../../authentication/service/auth.service';
 export class NavbarComponent implements OnInit {
 
   isLoggedIn: boolean = false;
+  eventBusSub?: Subscription;
 
   constructor(private storageService: StorageService,
-              private authService: AuthService) {
+              private authService: AuthService,
+              private eventBusService: EventBusService) {
   }
 
   ngOnInit() {
     this.isLoggedIn = this.storageService.isLoggedIn();
+
+    this.eventBusSub = this.eventBusService.on('logout', () => {
+      this.onClickLogout();
+    });
   }
 
   onClickLogout() {
